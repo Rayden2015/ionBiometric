bfaad78e9c7dbc3cb32f28c78a590a7855f835b7
=Nurudin Lartey <nurundin2010@gmail.com>
2020-12-30 02:38:57 +0000

    manual unlock with pin and biometric

--- src/app/locked/locked.page.ts
+++ src/app/locked/locked.page.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Plugins } from '@capacitor/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+import { ModalController } from "@ionic/angular";
 const { BiometricAuth } = Plugins;
 
 @Component({
-  selector: 'app-locked',
-  templateUrl: './locked.page.html',
-  styleUrls: ['./locked.page.scss'],
+  selector: "app-locked",
+  templateUrl: "./locked.page.html",
+  styleUrls: ["./locked.page.scss"],
 })
 export class LockedPage implements OnInit {
   showFallback = true;
-  password = '1234';
+  password = "1234";
   hasBiometricAuth = false;
 
   constructor(private modalCtrl: ModalController) {}
@@ -25,13 +25,16 @@ export class LockedPage implements OnInit {
   }
 
   async openBiometricAuth() {
+    console.log("openBiometricAuth");
     const authResult = await BiometricAuth.verify({
-      reason: 'Your session timed out',
-      title: 'Your session timed out',
+      reason: "Your session timed out",
+      title: "Your session timed out",
     });
 
     if (authResult.verified) {
       this.dismissLockScreen();
+    } else {
+      alert("Biometric Verfication Failed");
     }
   }
 
@@ -40,8 +43,11 @@ export class LockedPage implements OnInit {
   }
 
   unlockScreen() {
-    if (this.password == '1234') {
+    console.log("Unlock Screen");
+    if (this.password === "1234") {
       this.dismissLockScreen();
+    } else {
+      alert("Invalid password");
     }
   }
 }
